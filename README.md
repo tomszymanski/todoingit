14ea7b7e881d810e61ec731731b6114a202ad956
Tom Szymanski <tom.szymanski@sendgrid.com>
2013-11-01 13:57:30 -0600

    pulling out sort from list

--- README.md
+++ README.md
@@ -13,25 +13,5 @@ $ ruby todoingit.rb complete <task_id>
 $ ruby todoingit.rb list:outstanding
 $ ruby todoingit.rb list:completed
 $ ruby todoingit.rb tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>
-
-# To Be Impletemented
 $ ruby todoingit.rb filter:<tag_name>
 ```
-
-filter command
---------------
-
-```
-#This should display a list of all tasks with the personal tag sorted by creation date.
-$ ruby todoingit.rb list
-1. Eat some pie
-2. Walk the dog
-
-$ ruby todoingit.rb tag 2 personal pet-care
-Tagging task "Walk the dog" with tags: personal, pet-care
-
-$ ruby todoingit.rb filter:personal
-2. Walk the dog [personal]
-```
-
-
--- app/useraction.rb
+++ app/useraction.rb
@@ -14,23 +14,17 @@ class UserAction
     list
   end
 
-  def self.list(list_ordering = String.new,
+  def self.list(list_ordering,
                 tag_to_filter_on = String.new)
-    list_of_todos = Action.list
     display_list = ''
 
-    sorted_list_of_todos = list_of_todos.sort do |todo_a,todo_b|
-      case list_ordering
-      when 'outstanding'      
-        todo_b.creation_date <=> todo_a.creation_date
-      when 'completed'
-        todo_b.completion_date <=> todo_a.completion_date
-      else
-        list_of_todos.index(todo_a) <=> list_of_todos.index(todo_b)
-      end
+    if !list_ordering.nil?
+      list_of_todos = sort(list_ordering)
+    else
+      list_of_todos = Action.list
     end
 
-    sorted_list_of_todos.each do |todo_object|
+    list_of_todos.each do |todo_object|
       display_num = list_of_todos.index(todo_object)+1
       if !tag_to_filter_on.empty?
         if todo_object.tag.any? { |filter| filter == tag_to_filter_on }
@@ -43,6 +37,21 @@ class UserAction
 
     puts display_list
 
+  end
+
+  def self.sort(list_ordering)
+    list_of_todos_to_sort = Action.list
+    sorted_list_of_todos = list_of_todos_to_sort.sort do |todo_a,todo_b|
+      case list_ordering
+      when 'outstanding'      
+        todo_b.creation_date <=> todo_a.creation_date
+      when 'completed'
+        todo_b.completion_date <=> todo_a.completion_date
+      else
+        list_of_todos_to_sort.index(todo_a) <=> list_of_todos_to_sort.index(todo_b) 
+      end
+    end
+    sorted_list_of_todos
   end
 
   def self.tag(index,array_of_tags)
