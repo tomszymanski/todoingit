9c479687ce512e44b9284d9d8ec84f8f69e4fab8
Tom Szymanski <tom.szymanski@sendgrid.com>
2013-11-08 09:35:40 -0700

    touching up spec_helper

--- test/todo_spec.rb
+++ test/todo_spec.rb
@@ -1,11 +1,9 @@
-require 'minitest/unit'
-require 'minitest/autorun'
-require_relative '../app/todo'
+require_relative 'spec_helper.rb'
 
 class TestTodo < MiniTest::Unit::TestCase
   def setup
     @NEW_TODO_DESCRIPTION = "Bring Back That Loving Feeling"
-    
+
     @todo = Todo.new(@NEW_TODO_DESCRIPTION)
   end
   def test_description
